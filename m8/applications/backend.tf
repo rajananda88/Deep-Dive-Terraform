232f968d5a4bfedbbd7567ff35874ecb0655bec2
Virendra Kumar <virendraraj2009@gmail.com>
2022-03-29 15:09:42 +0530

    Update backend.tf
    Consul Address Changes

--- m8/applications/backend.tf
+++ m8/applications/backend.tf
@@ -3,7 +3,7 @@
 ##################################################################################
 terraform {
   backend "consul" {
-    address = "host.docker.internal:8500"
+    address = "192.168.0.111:8500"
     scheme  = "http"
   }
 }
